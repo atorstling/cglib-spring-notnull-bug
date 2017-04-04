38719614f4f8c40166b196ad71017d3d799889e0
Alexander Torstling <alexander@torstling.com>
2017-04-04 14:58:20 +0200

    comment

--- src/main/java/com/broken/UserRepo.java
+++ src/main/java/com/broken/UserRepo.java
@@ -3,5 +3,6 @@ package com.broken;
 import org.jetbrains.annotations.NotNull;
 
 public interface UserRepo<T extends User<I>, I> {
+    //Change return type between boxed Boolean and primitive boolean for different exceptions
     Boolean update(@NotNull T T);
 }
